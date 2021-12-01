d0fa12379fffc52c841c730fa11a151263e0879b
panlinnuo <58615088+panlinnuo@users.noreply.github.com>
2021-12-01 14:31:31 +0800

    Add files via upload

---
+++ precache-manifest.682b2b4c58d181c9f6d915a3ee1268d6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "50db2191a8a5e6dca46a",
+    "url": "/umi.6f4e94d1.css"
+  },
+  {
+    "revision": "50db2191a8a5e6dca46a",
+    "url": "/umi.6e6977a6.js"
+  },
+  {
+    "revision": "edb1eb12b2504d3cbcfe63e74f0827ee",
+    "url": "/static/emptyBg.edb1eb12.svg"
+  },
+  {
+    "revision": "60b7842a0f277928ce82a2e9de3a3a49",
+    "url": "/static/strLogo.60b7842a.png"
+  },
+  {
+    "revision": "ce966efbaf253dfc7684452c30171e3c",
+    "url": "/static/login.ce966efb.png"
+  },
+  {
+    "revision": "6934e4e36eb9c6313cbf626c5a190d18",
+    "url": "/static/方正汉真广标简体.6934e4e3.ttf"
+  },
+  {
+    "revision": "33925f146340a02ad237c1d8c620967f",
+    "url": "/static/btn.33925f14.png"
+  },
+  {
+    "revision": "5f80c8d1ce847463ce2b95bf8462441d",
+    "url": "/static/coupon.5f80c8d1.svg"
+  },
+  {
+    "revision": "f95e9aeab851b308cd384139cca9ab6b",
+    "url": "/static/StrategySquare.f95e9aea.png"
+  },
+  {
+    "revision": "8c30414cfad86e60d99b721f57d558cb",
+    "url": "/static/success.8c30414c.png"
+  },
+  {
+    "revision": "d74e64195485d1f3de0a7d7a82f63d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae7e77ccb9240dd537c80acf567669e",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "39a4f3ddc5d58403ef8b8315b79808cd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/pwacompat.min.js"
+  }
+];
